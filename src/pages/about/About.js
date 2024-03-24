3527d48fab4fcc158857f76a23f0ff94072647bc
JunLee8108 <lejhn1@gmail.com>
2024-03-24 00:29:54 -0500

    about page content added

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,33 +2,52 @@ import { motion } from "framer-motion";
 import "./About.css";
 
 export default function About() {
-  // 여기에 코드 타이핑 애니메이션을 위한 텍스트를 정의합니다.
-  const codeText = "Hello, I'm Jeong Hyun Lee";
+  const codeText = "About Me";
 
-  // 문자별로 나누어 각각에 대한 애니메이션을 적용합니다.
   const textMotionProps = {
     initial: { opacity: 0 },
     animate: (custom) => ({
       opacity: 1,
-      transition: { delay: custom * 0.02 }, // 각 글자마다 약간의 딜레이를 줍니다.
+      transition: { delay: custom * 0.08 },
     }),
   };
 
   return (
-    <div className="aboutContainer">
-      <motion.div className="typingAnimation">
-        {codeText.split("").map((char, index) => (
-          <motion.span
-            key={index}
-            custom={index}
-            variants={textMotionProps}
-            initial="initial"
-            animate="animate"
-          >
-            {char}
-          </motion.span>
-        ))}
-      </motion.div>
-    </div>
+    <>
+      <div className="about">
+        <motion.div className="about-typing-animation">
+          {codeText.split("").map((char, index) => (
+            <motion.span
+              key={index}
+              custom={index}
+              variants={textMotionProps}
+              initial="initial"
+              animate="animate"
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.div>
+      </div>
+
+      <div className="about-section animation-slow">
+        <div className="about-photo">
+          <img src="/imgs/JunLee.webp" alt="Jeong Hyun Lee" />
+          <h2>Jeong Hyun Lee</h2>
+          {/* Replace 'path-to-your-photo.jpg' with the actual path to your photo */}
+        </div>
+        <div className="about-description">
+          <p>
+            As a dedicated Computer Science student at Texas A&M University, I
+            have honed my full-stack development skills through a blend of
+            academic rigor and extensive hands-on experience. My technical
+            repertoire includes advanced proficiency in C++ and Python,
+            alongside substantial project work in web development using React,
+            Next.js, Node.js, Vue.js, TypeScript and MongoDB, as well as desktop
+            application development with Electron.js.
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
