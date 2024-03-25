cc64594ffaa32daf19c7137643692e653c0ab4d7
JunLee8108 <lejhn1@gmail.com>
2024-03-25 18:02:14 -0500

    about page content added

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,22 +1,71 @@
-import { motion } from "framer-motion";
 import "./About.css";
+
+import { motion } from "framer-motion";
 
 export default function About() {
   const codeText = "About Me";
+  const codeText2 = "Education";
 
   const textMotionProps = {
     initial: { opacity: 0 },
     animate: (custom) => ({
       opacity: 1,
-      transition: { delay: custom * 0.08 },
+      transition: { delay: custom * 0.04 },
     }),
   };
 
+  const sectionVariants = {
+    visible: { opacity: 1, y: 0, transition: { duration: 1, delay: 0.5 } },
+    hidden: { opacity: 0, y: 30 },
+  };
+
   return (
-    <>
-      <div className="about">
+    <div className="about">
+      <div className="about-me">
+        <div className="about-typing-animation-container">
+          <motion.div className="about-typing-animation">
+            {codeText.split("").map((char, index) => (
+              <motion.span
+                key={index}
+                custom={index}
+                variants={textMotionProps}
+                initial="initial"
+                animate="animate"
+              >
+                {char}
+              </motion.span>
+            ))}
+          </motion.div>
+        </div>
+
+        <motion.section
+          className="about-me-section animation-slow"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={sectionVariants}
+        >
+          <div className="about-me-photo">
+            <img src="/imgs/JunLee.webp" alt="Jeong Hyun Lee" />
+            <h2>Jeong Hyun Lee</h2>
+          </div>
+          <div className="about-me-description">
+            <p>
+              As a dedicated Computer Science student at Texas A&M University, I
+              have honed my full-stack development skills through a blend of
+              academic rigor and extensive hands-on experience. My technical
+              repertoire includes advanced proficiency in C++ and Python,
+              alongside substantial project work in web development using React,
+              Next.js, Node.js, Vue.js, TypeScript and MongoDB, as well as
+              desktop application development with Electron.js.
+            </p>
+          </div>
+        </motion.section>
+      </div>
+
+      <div className="about-typing-animation-container">
         <motion.div className="about-typing-animation">
-          {codeText.split("").map((char, index) => (
+          {codeText2.split("").map((char, index) => (
             <motion.span
               key={index}
               custom={index}
@@ -30,13 +79,18 @@ export default function About() {
         </motion.div>
       </div>
 
-      <div className="about-section animation-slow">
-        <div className="about-photo">
-          <img src="/imgs/JunLee.webp" alt="Jeong Hyun Lee" />
-          <h2>Jeong Hyun Lee</h2>
-          {/* Replace 'path-to-your-photo.jpg' with the actual path to your photo */}
+      <motion.section
+        className="about-me-section animation-slow"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        variants={sectionVariants}
+      >
+        <div className="about-me-photo">
+          <img src="/imgs/tam-logo.png" alt="Texas A&M University" />
+          <h2>Texas A&M University</h2>
         </div>
-        <div className="about-description">
+        <div className="about-me-description">
           <p>
             As a dedicated Computer Science student at Texas A&M University, I
             have honed my full-stack development skills through a blend of
@@ -47,7 +101,7 @@ export default function About() {
             application development with Electron.js.
           </p>
         </div>
-      </div>
-    </>
+      </motion.section>
+    </div>
   );
 }
--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,9 +1,76 @@
+import { motion } from "framer-motion";
 import "./Portfolio.css";
 
 export default function Portfolio() {
+  // 이 배열은 당신의 프로젝트를 나타냅니다. 실제 데이터로 바꿔주세요.
+  const projects = [
+    {
+      id: 1,
+      title: "Project 1",
+      description: "This is a description for Project 1.",
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      description: "This is a description for Project 2.",
+    },
+    {
+      id: 3,
+      title: "Project 3",
+      description: "This is a description for Project 3.",
+    },
+    // 추가 프로젝트
+  ];
+
+  const hoverVariants = {
+    hover: {
+      scale: 1.05,
+      transition: {
+        duration: 0.3, // 호버 애니메이션의 지속 시간
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  // Framer motion animation variants
+  const cardVariants = {
+    offscreen: {
+      y: 50,
+      opacity: 0,
+    },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  };
+
   return (
     <>
-      <h1>Portfolio</h1>
+      <h1 className="portfolio-title">My Portfolio</h1>
+      <div className="portfolio-container">
+        {projects.map((project) => (
+          <motion.div
+            key={project.id}
+            className="project-card"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.8 }}
+            variants={cardVariants}
+            whileHover={{
+              translateY: -30,
+              transition: { ease: "linear", duration: 2 },
+            }}
+          >
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+          </motion.div>
+        ))}
+      </div>
     </>
   );
 }
